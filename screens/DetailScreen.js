757eaf6dba5395cf1a8aa6da5e7561cb34ceb2da
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-11-26 13:03:20 -0500

    updated that shit

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -97,7 +97,7 @@ const DetailScreen = ({ route }) => {
               <TextInput
                 style={styles.textInput}
                 onChangeText={handleChange("employer")}
-                onBlur={handleBlur("employerrrr")}
+                onBlur={handleBlur("employerrrrerer")}
                 value={values.employer}
               />
 
