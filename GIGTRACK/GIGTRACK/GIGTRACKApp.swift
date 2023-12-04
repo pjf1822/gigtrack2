88bd0d773de479f0306f259e81e409e240c4c40d
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-12-04 13:10:40 -0500

    ipad changes

---
+++ GIGTRACK/GIGTRACK/GIGTRACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIGTRACKApp.swift
+//  GIGTRACK
+//
+//  Created by Peter Forbes on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GIGTRACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
